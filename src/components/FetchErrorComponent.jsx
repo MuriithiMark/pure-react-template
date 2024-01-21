402e5171be925962dc305637d0b646a8bd2ffdfd
MuriithiMark <markmwangi14317@gmail.com>
2024-01-21 12:08:57 +0300

    added interactivty components

---
+++ src/components/FetchErrorComponent.jsx
@@ -0,0 +1,8 @@
+export default function FetchErrorComponent() {
+    return (
+        <div className="interactive error">
+            <span>An Error Was encountered!</span>
+            <a href="/">Try Again</a>
+        </div>
+    )
+}
---
+++ src/components/LoadingComponent.jsx
@@ -0,0 +1,9 @@
+export default function LoadingComponent() {
+    return (
+        <div class="interactive loading">
+            <div className="outer-circle">
+                <div className="inner-circle"></div>
+            </div>
+        </div>
+    )
+}
